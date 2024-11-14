6e4a12ddce4f07c603161386d790f1125cc99757
Maya Josifovska <mayajosifovska@Mayas-MacBook-Pro-2.local>
2024-11-14 13:11:00 -0800

    basic dj name generating script

---
+++ src/app/getDJName.ts
@@ -0,0 +1,22 @@
+//import { topGenre } from "../app/spotify"; // fill in based on alex's code
+const topGenre = "indie"
+const genreDJNames: { [genre: string]: string[] } = {
+    pop: ["Poptart", "Glitter", "Mood"],
+    rap: ["Hustle", "Flow"],
+    indie: ["Gatekeeper", "Fern", "Moon"]
+};
+export const getDJName = async (): Promise<string | null> => {
+    if (topGenre) {
+        if (genreDJNames[topGenre]) {
+            const randomIndex = Math.floor(Math.random() * genreDJNames[topGenre].length);
+        const name = "DJ " + genreDJNames[topGenre][randomIndex];
+        return name;
+    } else {
+        console.log("genre not in list");
+        return  "DJ Obscure";
+    }
+    } else {
+        console.log("API call failed");
+        return "DJ Obscure";
+    }
+};
